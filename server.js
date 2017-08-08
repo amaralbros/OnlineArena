c8b840eea7f237b8f006132aa6d21105fe0e51f1
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-08-08 13:08:28 -0700

    Change Port

--- server.js
+++ server.js
@@ -71,6 +71,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
+const PORT = process.env.PORT || 3000;
 
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
@@ -84,7 +85,7 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
 
-server.listen(8081,function(){ // Listens to port 8081
+server.listen(PORT,function(){ // Listens to port 8081
     console.log('Listening on '+server.address().port);
 });
 
