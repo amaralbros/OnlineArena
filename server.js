8f62d624f25dba1b245f27633ee1cc9920aea958
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-08-07 21:31:23 -0700

    Setup app config

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+var app = express();
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js'));
+app.use('/assets',express.static(__dirname + '/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
