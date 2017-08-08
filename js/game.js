4d7d81ad1aa9aaf77712950a084f98aeff84b859
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-08-07 18:07:18 -0700

    Initial Setup

---
+++ js/game.js
@@ -0,0 +1,49 @@
+
+var Game = {};
+
+Game.init = function(){
+    game.stage.disableVisibilityChange = true;
+};
+
+Game.preload = function() {
+    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
+    game.load.image('sprite','assets/sprites/sprite.png'); // this will be the sprite of the players
+};
+
+Game.create = function(){
+    Game.playerMap = {};
+    var map = game.add.tilemap('map');
+    map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
+    var layer;
+    for(var i = 0; i < map.layers.length; i++) {
+        layer = map.createLayer(i);
+    }
+    layer.inputEnabled = true; // Allows clicking on the map
+    Client.askNewPlayer();
+
+    layer.events.onInputUp.add(Game.getCoordinates, this);
+
+};
+
+Game.getCoordinates = function(layer,pointer){
+    Client.sendClick(pointer.worldX,pointer.worldY);
+};
+
+Game.addNewPlayer = function(id,x,y){
+    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+};
+
+Game.removePlayer = function(id){
+    Game.playerMap[id].destroy();
+    delete Game.playerMap[id];
+};
+
+Game.movePlayer = function(id,x,y){
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+    var duration = distance*10;
+    var tween = game.add.tween(player);
+    tween.to({x:x,y:y}, duration);
+    tween.start();
+};
