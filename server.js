b1c8ca335fa3cf03323d87b4b050f3a52abf1561
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-08-07 21:33:13 -0700

    Add Login/Logout Functionality

--- server.js
+++ server.js
@@ -27,7 +27,43 @@ const io = socketIO(server);
 
 //LOGIN/LOGOUT
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  // console.log('Client connected');
+    socket.on('newplayer',function(){
+        socket.player = {
+            id: server.lastPlayderID++,
+            x: randomInt(100,400),
+            y: randomInt(100,400)
+        };
+        socket.emit('allplayers',getAllPlayers());
+        socket.broadcast.emit('newplayer',socket.player);
+
+        socket.on('click',function(data){
+            socket.player.x = data.x;
+            socket.player.y = data.y;
+            io.emit('move',socket.player);
+        });
+
+        socket.on('disconnect',function(){
+            io.emit('remove',socket.player.id);
+        });
+    });
+
+  // socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+function getAllPlayers(){
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+        var player = io.sockets.connected[socketID].player;
+        if(player) players.push(player);
+    });
+    return players;
+}
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+
+
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
